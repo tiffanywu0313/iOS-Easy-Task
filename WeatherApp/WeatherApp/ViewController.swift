4396e7e485ba121dd57991eef8867ff6eaded04a
Tiffany Wu <39014470+tiffanywu0313@users.noreply.github.com>
2018-09-08 16:45:32 -0700

    Update ViewController.swift

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let apiKey = "e32b05a8bf114a505587728b7c94ee6a"
+    @IBOutlet var lblLabel: UILabel!
+    
+    @IBAction func Click (sender: AnyObject){
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=29.7604&lon=95.3698&appid=\(apiKey)).responseJSON{}
+            
+        lblLabel.text = ""
+    }
+    @IBOutlet weak var Click: UIButton!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
